5581fc5b54ac0e5ba70752c1f7688d07460a882a
Thiimo <thimosietsma@gmail.com>
2021-01-13 13:21:49 +0100

    Added API RestClient & gitignore :fire:

---
+++ corono-app/RestClient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace corono_app
+{
+    public enum httpVerb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    class RESTClient
+    {
+        public string endPoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+
+        public RESTClient()
+        {
+            endPoint = "";
+            httpMethod = httpVerb.GET;
+        }
+
+        public string makeRequest()
+        {
+
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = httpMethod.ToString();
+            request.Host = "demo.recras.nl";
+            request.Headers.Add("Authorization", "Bearer ktgz4992zI47ElVAFzExtXPwiurhH_l9");
+            HttpWebResponse response = null;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                strResponseValue = "{\"errorMessages\":[\"" + ex.Message.ToString() + "\"],\"errors\":{}}";
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    ((IDisposable)response).Dispose();
+                }
+            }
+
+            return strResponseValue;
+        }
+    }
+}
