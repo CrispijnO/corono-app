10f0401b0ff57047017f54063caee8e4ad972c1e
Thiimo <thimosietsma@gmail.com>
2021-01-20 10:34:46 +0100

    Added API Calls :fire:

--- corono-app/Form1.cs
+++ corono-app/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,38 +18,107 @@ namespace corono_app
         public Form1()
         {
             InitializeComponent();
-            init();
+            List<coronaApi> coronaStats = getCoronaStats();
+            handleApplication(coronaStats, "8447AA", 1200);
         }
-
-        private void init()
+        private List<coronaApi> getCoronaStats()
         {
-
             RESTClient client = new RESTClient();
-            /*
-             CITY API
-            client.endPoint = "https://wft-geo-db.p.rapidapi.com/v1/geo/countries/NL/regions/FR/cities";
-            string[] headers1 = new string[2] {"x-rapidapi-key", "e6fba40ecbmsh53e0c124daa7214p19b766jsn4209defa38c2" };
-            string[] headers2 = new string[2] { "x-rapidapi-host", "wft-geo-db.p.rapidapi.com" };
-            string[][] headers = new string[][] { headers1, headers2 };
-            client.headers = headers;
-            client.host = "wft-geo-db.p.rapidapi.com";
-            */
-            /*
-             POST CODE API
-            client.endPoint = "https://api.spikkl.nl/geo/nld/lookup.json?postal_code=2611HB&filter=postal_code,street_name,city,municipality,administrative_areas,country&key=2e6a697089bb6b7ae817737d21d080dd";
-            client.host = "api.spikkl.nl";
-            string[][] headers = new string[][] { };
-            client.headers = headers;
-            */
-            /*
-             Corona statistieken API
             client.endPoint = "https://geodata.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json";
             client.host = "geodata.rivm.nl";
-            string[][] headers = new string[][] { };
-            client.headers = headers;
-            */
+            client.headers = new string[][] { };
             string response = client.makeRequest();
-            Console.WriteLine(response);
+            List<coronaApi> coronaStatsRes = JsonConvert.DeserializeObject<List<coronaApi>>(response);
+            List<coronaApi> coronaStats = new List<coronaApi>();
+            bool existDateNow = coronaStats.Exists(d => d.Date_of_report.Date == DateTime.Now.AddDays(-1).Date);
+            coronaStatsRes.ForEach(res =>
+            {
+                if (res.Date_of_report.Date == (existDateNow ? DateTime.Now.Date : DateTime.Now.AddDays(-1).Date))
+                {
+                    coronaStats.Add(res);
+                    return;
+                }
+                return;
+            });
+            return coronaStats;
+        }
+
+        private async void handleApplication(List<coronaApi> coronaStats, string postCode, int visitedCount)
+        {
+            RESTClient client = new RESTClient();
+            string postcode = postCode; // hard coded.
+            client.endPoint = $"https://api.spikkl.nl/geo/nld/lookup.json?postal_code={postcode}&filter=postal_code,street_name,city,municipality,administrative_areas,country&key=2e6a697089bb6b7ae817737d21d080dd";
+            client.host = "api.spikkl.nl";
+            client.headers = new string[][] { };
+            string response = client.makeRequest();
+            postCodeApi postCodeResults = JsonConvert.DeserializeObject<postCodeApi>(response);
+            List<string> cities = new List<string>();
+            postCodeResults.results.ForEach(res =>
+            {
+                if (res.postal_code != postcode) { return; }
+                int pos = cities.IndexOf(res.city);
+                if (pos == -1)
+                {
+                    cities.Add(res.city);
+                }
+            });
+            int citizenCount = 0;
+            List<string> cityCodes = new List<string>();
+            for (int i = 0; i < cities.Count;)
+            {
+                string city = cities[i];
+                client.endPoint = $"https://wft-geo-db.p.rapidapi.com/v1/geo/countries/NL/regions/FR/cities?namePrefix={city}";
+                client.host = "wft-geo-db.p.rapidapi.com";
+                string[] apiKey = new string[2] { "x-rapidapi-key", "e6fba40ecbmsh53e0c124daa7214p19b766jsn4209defa38c2" };
+                string[] host = new string[2] { "x-rapidapi-host", "wft-geo-db.p.rapidapi.com" };
+                string[][] headers = new string[][] { apiKey, host };
+                client.headers = headers;
+                response = client.makeRequest();
+                CityApi cityApiRes = JsonConvert.DeserializeObject<CityApi>(response);
+                cityApiRes.data.ForEach(data =>
+                {
+                    if (cityCodes.IndexOf(data.wikiDataId) == -1)
+                    {
+                        cityCodes.Add(data.wikiDataId);
+                    }
+                });
+                await Task.Delay(1000);
+                i++;
+            };
+            await Task.Delay(1000); // handeling the 1 request per second :(
+            for (int i = 0; i < cityCodes.Count;)
+            {
+                string cityCode = cityCodes[i];
+                client.endPoint = $"https://wft-geo-db.p.rapidapi.com/v1/geo/cities/{cityCode}";
+                client.host = "wft-geo-db.p.rapidapi.com";
+                string[] apiKey = new string[2] { "x-rapidapi-key", "e6fba40ecbmsh53e0c124daa7214p19b766jsn4209defa38c2" };
+                string[] host = new string[2] { "x-rapidapi-host", "wft-geo-db.p.rapidapi.com" };
+                string[][] headers = new string[][] { apiKey, host };
+                client.headers = headers;
+                response = client.makeRequest();
+                Console.WriteLine(response);
+                CityPopulation population = JsonConvert.DeserializeObject<CityPopulation>(response);
+                citizenCount += population.data.population;
+                Console.WriteLine(population.data.population + " " + citizenCount);
+                // Waiting 1 second for the API rate limit.
+                await Task.Delay(1000);
+                //
+                i++;
+            }
+            int coronaStatCount = 0;
+            coronaStats.ForEach(stat =>
+            {
+                if (cities.Contains(stat.Municipality_name))
+                {
+                    coronaStatCount = stat.Total_reported;
+                }
+            });
+            handlePercentages(citizenCount, coronaStatCount, visitedCount);
+        }
+
+        private void handlePercentages(int citizenCount, int coronaStatCount, int visitedCount)
+        {
+            // todo.
         }
 
         private void label2_Click(object sender, EventArgs e)
