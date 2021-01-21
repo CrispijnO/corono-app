9b7998a2434cd037a4ec41240eab1108d4b95f89
Marthijs1 <75075864+Marthijs1@users.noreply.github.com>
2021-01-21 12:41:45 +0100

    Test kleur van panel

--- corono-app/Form1.cs
+++ corono-app/Form1.cs
@@ -140,21 +140,24 @@ namespace corono_app
             if (chanceInfection >= 15)
             {
                 Console.WriteLine("Its higher than 15%");
+                panel1.BackColor = Color.Black;
             }
             else if (chanceInfection >= 5 && chanceInfection < 15)
             {
                 Console.WriteLine("Its between 5 and 15%");
+                panel1.BackColor = Color.Red;
 
             }
             else if (chanceInfection >= 1 && chanceInfection < 5)
             {
                 Console.WriteLine("Its between 1 and 5%");
-
+                panel1.BackColor = Color.Orange;
 
             }
             else if (chanceInfection < 1)
             {
                 Console.WriteLine("Its lower than 1%");
+                panel1.BackColor = Color.Green;
 
             }
         }
@@ -163,7 +166,7 @@ namespace corono_app
         {
           
         }
-
+        *
             private void txtbox_TextChanged(object sender, EventArgs e)
         {
 
